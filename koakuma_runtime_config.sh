1940a85e64585c657c1d14ef2452c1700e257332
rawhide kobayashi <rawhide@neet.works>
2026-01-05 23:45:46 -0600

    add runtime config script for koakuma + change network settings

--- koakuma_runtime_config.sh
+++ koakuma_runtime_config.sh
@@ -2,23 +2,25 @@
 set -euo pipefail
 
 echo "Copying network interface configuration..."
+rm -rfv /etc/systemd/network/*
 install -vm644 config/hosts/koakuma/networkd/* /etc/systemd/network/
 
 echo "Restarting networking services..."
 systemctl restart systemd-networkd.service
 systemctl restart systemd-resolved.service
-systemctl restart sshd.service
 
 echo "Installing pacman hooks/scripts..."
-install -vm644 config/common/pacman/hooks/* /mnt/etc/pacman.d/hooks/
-install -vm744 config/common/pacman/scripts/* /mnt/etc/pacman.d/scripts/
+rm -rfv /etc/pacman.d/hooks/*
+install -vm644 config/common/pacman/hooks/* /etc/pacman.d/hooks/
+rm -rfv /etc/pacman.d/scripts/*
+install -vm744 config/common/pacman/scripts/* /etc/pacman.d/scripts/
 
 echo "Installing miscellaneous config drop-ins..."
-
-install -vm644 config/common/cmdline.d/* /mnt/etc/cmdline.d/
-mkdir -vp /mnt/etc/modprobe.d
-install -vm644 config/hosts/koakuma/modprobe.d/* /mnt/etc/modprobe.d/
+rm -rfv /etc/cmdline.d/*
+install -vm644 config/common/cmdline.d/* /etc/cmdline.d/
+rm -rfv /etc/modprobe.d/*
+install -vm644 config/hosts/koakuma/modprobe.d/* /etc/modprobe.d/
 
 echo "Modifying mkinitcpio.conf for ZFS..."
-sed -i 's/MODULES=()/MODULES=(zfs)/' /mnt/etc/mkinitcpio.conf
-sed -i 's/HOOKS=(base .*/HOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block zfs filesystems)/' /mnt/etc/mkinitcpio.conf
+sed -i 's/MODULES=()/MODULES=(zfs)/' /etc/mkinitcpio.conf
+sed -i 's/HOOKS=(base .*/HOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block zfs filesystems)/' /etc/mkinitcpio.conf
