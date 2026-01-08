1df6573aa91ef9ce356565e16839db4f60a4ba0b
rawhide kobayashi <rawhide@neet.works>
2026-01-07 23:37:49 -0600

    install doesn't work with directories? who needs it? + typo

--- koakuma_podman_config.sh
+++ koakuma_podman_config.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 echo "Copying quadlets..."
 rm -rfv /etc/containers/systemd/*
-install -vdm644 config/hosts/koakuma/podman/quadlets/* /etc/containers/systemd/
+cp -rv config/hosts/koakuma/podman/quadlets/* /etc/containers/systemd/
 
-echo "Reloading systemctl daemon...
+echo "Reloading systemctl daemon..."
 systemctl daemon-reload
