e60a009dc8f73e58cbfc4e0609768ef7bd7cd13c
rawhide kobayashi <rawhide@neet.works>
2026-01-10 18:38:58 -0600

    proper permissions for files copied to the kanidm volume

--- config/hosts/koakuma/letsencrypt/hooks/deploy/kanidm.sh
+++ config/hosts/koakuma/letsencrypt/hooks/deploy/kanidm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-install -vm640 /etc/letsencrypt/live/koakuma.idm.neetworks-auth.com/fullchain.pem /var/lib/containers/storage/volumes/kanidm/
-install -vm640 /etc/letsencrypt/live/koakuma.idm.neetworks-auth.com/privkey.pem /var/lib/containers/storage/volumes/kanidm/
+install -vm600 -o100000 -g100000 /etc/letsencrypt/live/koakuma.idm.neetworks-auth.com/fullchain.pem /var/lib/containers/storage/volumes/kanidm/
+install -vm600 -o100000 -g100000 /etc/letsencrypt/live/koakuma.idm.neetworks-auth.com/privkey.pem /var/lib/containers/storage/volumes/kanidm/
