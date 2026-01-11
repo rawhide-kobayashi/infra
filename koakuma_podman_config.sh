85f6b0c49b9517da58e2ea4f04961659470cb32d
rawhide kobayashi <rawhide@neet.works>
2026-01-10 18:21:29 -0600

    add subgid

--- koakuma_podman_config.sh
+++ koakuma_podman_config.sh
@@ -8,8 +8,9 @@ cp -rv config/hosts/koakuma/podman/quadlets/* /etc/containers/systemd/
 echo "Reloading systemctl daemon..."
 systemctl daemon-reload
 
-echo "Copying subuid file..."
+echo "Copying subuid/gid file..."
 install -vm644 config/hosts/koakuma/subuid /etc/subuid
+install -vm644 config/hosts/koakuma/subgid /etc/subgid
 
 echo "Copying letsencrypt hooks..."
 install -vm744 config/hosts/koakuma/letsencrypt/hooks/deploy/* /etc/letsencrypt/renewal-hooks/deploy/
