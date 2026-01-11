3793db8188e18ee25db704d6ebb337696fa6a813
rawhide kobayashi <rawhide@neet.works>
2026-01-10 19:14:29 -0600

    actually the most correct permissions

--- config/hosts/koakuma/letsencrypt/hooks/deploy/kanidm.sh
+++ config/hosts/koakuma/letsencrypt/hooks/deploy/kanidm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-install -vm400 -o101000 -g101000 /etc/letsencrypt/live/koakuma.idm.neetworks-auth.com/fullchain.pem /var/lib/containers/storage/volumes/kanidm/
-install -vm400 -o101000 -g101000 /etc/letsencrypt/live/koakuma.idm.neetworks-auth.com/privkey.pem /var/lib/containers/storage/volumes/kanidm/
+install -vm644 -o100000 -g100000 /etc/letsencrypt/live/koakuma.idm.neetworks-auth.com/fullchain.pem /var/lib/containers/storage/volumes/kanidm/
+install -vm644 -o100000 -g100000 /etc/letsencrypt/live/koakuma.idm.neetworks-auth.com/privkey.pem /var/lib/containers/storage/volumes/kanidm/
