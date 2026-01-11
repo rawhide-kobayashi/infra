f602d9e5298529463133c2c111d8df11028fb0a8
rawhide kobayashi <rawhide@neet.works>
2026-01-10 19:18:11 -0600

    SIGHUP the kanidm server to reload certs on renewal

--- config/hosts/koakuma/letsencrypt/hooks/deploy/kanidm.sh
+++ config/hosts/koakuma/letsencrypt/hooks/deploy/kanidm.sh
@@ -3,3 +3,4 @@ set -euo pipefail
 
 install -vm644 -o100000 -g100000 /etc/letsencrypt/live/koakuma.idm.neetworks-auth.com/fullchain.pem /var/lib/containers/storage/volumes/kanidm/
 install -vm644 -o100000 -g100000 /etc/letsencrypt/live/koakuma.idm.neetworks-auth.com/privkey.pem /var/lib/containers/storage/volumes/kanidm/
+podman kill --signal SIGHUP kanidm-server
