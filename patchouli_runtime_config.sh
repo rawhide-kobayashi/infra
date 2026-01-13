110c2745d8a5cee68d56126851b5c961d4179901
rawhide kobayashi <rawhide@neet.works>
2026-01-13 00:35:02 -0600

    and put in the wrong folder name

--- patchouli_runtime_config.sh
+++ patchouli_runtime_config.sh
@@ -37,5 +37,5 @@ rm -rfv /etc/sudoers.d/*
 install -vm440 config/common/sudoers.d/* /etc/sudoers.d/
 
 echo "Install samba configuration..."
-install -vm644 config/hosts/koakuma/samba/smb.conf /etc/samba/smb.conf
+install -vm644 config/hosts/patchouli/samba/smb.conf /etc/samba/smb.conf
 systemctl restart smb
