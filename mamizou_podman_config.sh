dd6313b5eed3caf2fa7628af05f0ad41dec8ba38
rawhide kobayashi <rawhide@neet.works>
2026-01-21 16:36:30 -0600

    add mamizou podman config script

---
+++ mamizou_podman_config.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Copying quadlets..."
+rm -rfv /etc/containers/systemd/*
+cp -rv config/hosts/mamizou/podman/quadlets/* /etc/containers/systemd/
+
+echo "Reloading systemctl daemon..."
+systemctl daemon-reload
+
+echo "Copying subuid/gid file..."
+install -vm644 config/hosts/mamizou/subuid /etc/subuid
+install -vm644 config/hosts/mamizou/subgid /etc/subgid
+
+echo "Copying letsencrypt hooks..."
+install -vm744 config/hosts/mamizou/letsencrypt/hooks/deploy/* /etc/letsencrypt/renewal-hooks/deploy/
+
+echo "Copying kanidm config..."
+install -vm640 -o100000 -g101000 config/hosts/mamizou/podman/config/kanidm/server.toml /var/lib/containers/storage/volumes/kanidm/
