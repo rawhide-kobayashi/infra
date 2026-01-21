0e16bf8c190e5292c419b1a9fb8104072f5147f2
rawhide kobayashi <rawhide@neet.works>
2026-01-21 17:11:32 -0600

    TYPO

--- config/hosts/mamizou/letsencrypt/hooks/deploy/kanidm.sh
+++ config/hosts/mamizou/letsencrypt/hooks/deploy/kanidm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-install -vm640 -o100000 -g101000 /etc/letsencrypt/live/mamizous.idm.neetworks-auth.com/fullchain.pem /var/lib/containers/storage/volumes/kanidm/
-install -vm640 -o100000 -g101000 /etc/letsencrypt/live/mamizous.idm.neetworks-auth.com/privkey.pem /var/lib/containers/storage/volumes/kanidm/
+install -vm640 -o100000 -g101000 /etc/letsencrypt/live/mamizou.idm.neetworks-auth.com/fullchain.pem /var/lib/containers/storage/volumes/kanidm/
+install -vm640 -o100000 -g101000 /etc/letsencrypt/live/mamizou.idm.neetworks-auth.com/privkey.pem /var/lib/containers/storage/volumes/kanidm/
 podman kill --signal SIGHUP kanidm-server
